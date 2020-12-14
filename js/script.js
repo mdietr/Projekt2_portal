ad45ef627de5ea89493c243d88c6bbd02a443999
Magdalena Dietrich <madietrich90@gmail.com>
2020-12-14 21:38:58 +0100

    repository projek2 portal

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+$('[data-toggle="tooltip"]').tooltip();
+});
+
+$(document).ready(function(){
+    $('.carousel').carousel({
+        interval: 2000,
+        ride: "carousel"
+    });
+});
+
+//var carouselImageCaptions = document.querySelector('#carouselImageCaptions')
+//var carousel = new bootstrap.Carousel(carouselImageCaptions, {
+//});
